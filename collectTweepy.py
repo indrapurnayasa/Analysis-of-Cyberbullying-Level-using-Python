f6794b58121d26bbdaf1fdb6163ad6cb41005c25
indrapurnayasa <62170534+indrapurnayasa@users.noreply.github.com>
2022-09-06 17:49:27 +0800

    update code

--- collectTweepy.py
+++ collectTweepy.py
@@ -4,13 +4,13 @@ from pymongo import MongoClient
 from tweepy import OAuthHandler
 
 client = MongoClient('localhost', 27017)
-db = client['cyberbullyingMei']
-collection = db['cyberbullyingIdiot']
+db = client['databaseName']
+collection = db['collectionName']
 
-access_token = "250996215-cRefKugh80cVxnQATGMjoXvHGKf0oQ1CvEbQ42tu"
-access_token_secret = "plwBZRnSyrW2ugUQRHU7WOK7KzrHG6I8eCwunpk9gdazC"
-consumer_key = "OB1ur1Q3NumIvPkpcSc6haGc3"
-consumer_secret = "BzWvPKQy66q7o9N9NVyoTXYdjw100CGMybOvb19bbUc75M1zih"
+access_token = "xxxx"
+access_token_secret = "xxxx"
+consumer_key = "xxxx"
+consumer_secret = "xxxx"
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -19,7 +19,7 @@ qry = input("Masukkan Query yang akan anda cari :")
 countKeyword=100
 
 tweet = tweepy.Cursor(api.search, q=qry, lang="in",tweet_mode='extended',
-                      since='2022-05-01',until='2022-05-04',
+                      since='yyyy-mm-dd',until='yyyy-mm-dd', #max 7 days
                       count='countKeyword', wait_on_rate_limit=True).items(10000)
 
 for tweets in tweet:
