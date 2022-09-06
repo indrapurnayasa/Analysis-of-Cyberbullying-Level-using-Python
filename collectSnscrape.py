226090866ddf0705a8508317029bb0aff7e2b4ac
indrapurnayasa <62170534+indrapurnayasa@users.noreply.github.com>
2022-09-06 17:48:04 +0800

    update code

--- collectSnscrape.py
+++ collectSnscrape.py
@@ -5,18 +5,18 @@ from pymongo import MongoClient
 from tweepy import OAuthHandler
 
 client = MongoClient('localhost', 27017)
-db = client['cyberbullyingTwitter']
-collection = db['cyberbullying6Apr']
+db = client['databaseName']
+collection = db['collectionName']
 
-access_token = "250996215-QlydZXfPKlML21RTbcYbDTj82NmL2z4b0viP1wR8"
-access_token_secret = "EyBPBYxsATRhXs84mqVewA14yVxeHqtPWiMAieVPYvpRb"
-consumer_key = "FaMiOPzjvFZL9RvHYTq5eFlad"
-consumer_secret = "rxbssYK7uY6nQlglo3aVefQL7zJEcXILe9YmK5Aa5Tikz91Yfk"
+access_token = "xxxx"
+access_token_secret = "xxxx"
+consumer_key = "xxxx"
+consumer_secret = "xxxx"
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper('brengsek + since:2022-04-06 until:2022-04-07 lang:in').get_items()):
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper('keyword + since:yyyy-mm-dd until:yyyy-mm-dd lang:in').get_items()):
     id = tweet.id
     if(id==None or id==''):
         continue
